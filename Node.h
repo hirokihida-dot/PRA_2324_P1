c8b46035920a94143be92022ecfa7df5d545276b
Carlos Rams Aleixandre <cramale@upv.edu.es>
2025-10-21 20:06:25 +0200

    Añadida implementación de la clase Nodo

---
+++ Node.h
@@ -0,0 +1,20 @@
+#include <ostream>
+#include <iostream>
+
+template <typename T>
+
+class Node{
+
+    public:
+            T data;
+            Node<T>* next;
+            //constructor
+            Node(T data, Node<T>* next=nullptr): data(data), next(next){}
+            //Operador de salida para imprirmir el nodo
+            friend std::ostream& operator<<(std::ostream&out, const Node<T> &node){
+            std::cout<<node.data;
+            return std::cout;
+            }
+ };
+
+
